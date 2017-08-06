8ba6927caa21c4968267debc8f168564d2c8602f
Albert I <krascgq@outlook.co.id>
2017-08-06 20:59:15 +0800

    chtolly-ido: Spectrum: Reset profile to Balanced every reboot
    * Avoid getting stupid bug with profile
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- ramdisk/init.spectrum.sh
+++ ramdisk/init.spectrum.sh
@@ -3,6 +3,6 @@
 # Profile initialization script by nathanchance
 
 # If there is not a persist value or if the profile isn't balanced, we need to set one
-if [ ! -f /data/property/persist.spectrum.profile ]; then
+if [ ! -f /data/property/persist.spectrum.profile ] || [[ $(cat /data/property/persist.spectrum.profile) != 0 ]]; then
     setprop persist.spectrum.profile 0
 fi
